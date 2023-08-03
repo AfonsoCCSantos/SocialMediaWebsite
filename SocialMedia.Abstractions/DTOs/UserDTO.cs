5ff082096576e62f0884883ed502e9e362ea59e1
Afonso Santos <afonso.acc.santos@gmail.com>
2023-08-03 20:44:22 +0100

    Added UserDTO

---
+++ SocialMedia.Abstractions/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace SocialMedia.Abstractions.DTOs
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public int FollowersCount { get; set; }
+
+        public int FollowingCount { get; set; }
+
+        public int PostsCount { get; set; }
+    }
+}
