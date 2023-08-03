8dea789bed178d6fdcbe42438b41cea5ec3d8d20
Afonso Santos <afonso.acc.santos@gmail.com>
2023-08-03 20:34:46 +0100

    User saves password hash, not password

--- SocialMedia.Data/Models/User.cs
+++ SocialMedia.Data/Models/User.cs
@@ -12,7 +12,7 @@ namespace SocialMedia.Data.Models
 
         public string Email { get; set; } = null!;
 
-        public string Password { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
 
         public string? PhoneNumber { get; set; }
 
