a4370cce6fb7d877127f4be1aa32ced76f3464d5
Afonso Santos <afonso.acc.santos@gmail.com>
2023-08-03 21:47:36 +0100

    AuthenticationService receives dbcontext from DI

--- SocialMedia.Core/Services/AuthenticationService.cs
+++ SocialMedia.Core/Services/AuthenticationService.cs
@@ -1,7 +1,27 @@
-﻿namespace SocialMedia.Core.Services
+﻿using SocialMedia.Abstractions.Requests;
+using SocialMedia.Data.Data;
+
+namespace SocialMedia.Core.Services
 {
     public class AuthenticationService
     {
+
+        private readonly SocialMediaContext _context;
+
+        public AuthenticationService(SocialMediaContext context)
+        {
+            _context = context;
+        }
+
+        public int Register(RegisterUserRequest request)
+        {
+
+
+
+
+
+            return 0;
+        }
 
     }
 }
